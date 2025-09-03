3da14aa8b7c207a4296a17b207f9494da746a306
naiarasilva2 <naiara.osilva@aluno.edu.es.gov.br>
2025-09-03 11:06:26 -0300

    Create index.php

---
+++ aula02-3trimestre/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title> Aula 02 - PHP</title>
+    </head>
+    <body>
+        <?php
+        $nome1= "Naiara"; 
+        echo "meu nome e" .$nome1; 
+        ?> 
+    </body>
+</html>
