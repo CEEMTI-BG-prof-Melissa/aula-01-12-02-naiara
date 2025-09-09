f30ef0c4e1ab23d99ba421bcb5f0d43de395f12f
naiarasilva2 <naiara.osilva@aluno.edu.es.gov.br>
2025-09-09 11:10:39 -0300

    Create index.php

---
+++ aula03/3ºtrimestre/index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title> aula 02 php<title>
+    </head>
+    <body>
+        <?php
+        $some="Naiara";
+        $some="Deus";
+
+        $n1=10
+        $n2=2;
+        $soma= $n1+$n2;
+
+        echo "meu nome é" . $some1 . "e minha dupla é " .$some2;
+        echo "meu nome é" . $some1 . "e minha dupla é " .$some2;
+
+        echo "<br> <br> A soma de $n1 com $n2 é igual a soma $soma. "
+        ?>
+    </body>
+</html> 
