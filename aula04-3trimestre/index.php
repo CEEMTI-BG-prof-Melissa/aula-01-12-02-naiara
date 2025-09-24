f62d12955d95ad3a3837a98b8fb2782f944fea83
naiarasilva2 <naiara.osilva@aluno.edu.es.gov.br>
2025-09-24 08:36:42 -0300

    Create index.php

---
+++ aula04-3trimestre/index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title> Aula 04 PHP</title>
+    </head>
+    <body>
+        <?php
+            $n1=$_GET["a"];
+            $n2=$_GET["b"];
+
+            echo "<br></br>a soma de $n1 com $n2 e igual a " .($n1+$n2);
+            echo "<br></br>subtração de $n1 com $n2 e igual a" .($Sn1-n2);
+            echo "<br></br>a multiplicação de $n1 com $n2 e igual a" .($sn1*n2);
+            echo "<br></br>a divisão de $n1 com $n2 e igual a" .($n1/&n2);
+            echo "<br></br>o resto de $n1 com $n2 e igual a" .($n1%&n2);
+        ?>
+    </body>
+</html>
