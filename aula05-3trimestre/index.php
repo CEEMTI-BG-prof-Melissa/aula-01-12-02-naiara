19aab4fa083edc0b420e2c386e619891d0934270
naiarasilva2 <naiara.osilva@aluno.edu.es.gov.br>
2025-09-30 10:52:38 -0300

    Update index.php

--- aula05-3trimestre/index.php
+++ aula05-3trimestre/index.php
@@ -7,13 +7,16 @@
         $n1= 2;
         $n2= $_GET["x"];
 
-        echo "Os valores informados são $n1 e $n2 <br>";
-        echo " O valor absoluto de $n2 é " .abs($n2);
-        echo "<br> O resultado $n2 <sup> $n1</sup> é igual a  " .pow($n2,$n1);
-        echo " A  raiz quadrada de $n2 é " .sqrt($n2);
-        echo "<br> O número $n2 arredondado é " .round($n2);
-        echo "<br> O número $n2 arredondado para cima é " .ceil($n2);
-        echo "<br> O número $n2 arredondado para baixo é " .floor($n2);
+        echo "<br> Os valores informados são $n1 e $n2 <br>";
+        echo "<br> O valor absoluto de $n2 é " .abs($n2);
+        echo "<br> O resultado $n2 <sup> $n1</sup> é  " .pow($n2,$n1);
+        echo "<br> A  raiz quadrada de $n2 é igual á " .sqrt($n2);
+        echo "<br> O número $n2 arredondado é igual á" .round($n2);
+        echo "<br> O número $n2 arredondado para cima é igual á " .ceil($n2);
+        echo "<br> O número $n2 arredondado para baixo é igual á " .floor($n2);
+        echo "<br> A parte inteira de $n2 é igual á " .intval($n2);
+        echo "<br> Um pirulito custa R$ " .number_format($n1, 2);
+        echo "<br>"
         ?>
     </body>
 </html>
