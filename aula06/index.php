57322328ea85661150960af4b79dfb2f713a09e4
naiarasilva2 <naiara.osilva@aluno.edu.es.gov.br>
2025-09-30 11:09:52 -0300

    Create index.php

---
+++ aula06/index.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <title> Aula 06 - operadores de atribuição </title>
+    </head>
+    <body>
+        <h3>Operadores de Atribuição</h3>
+        <hr>
+
+        <?php
+            $a=2
+            $b=$a+5;
+
+            echo" A igual a $a e B é igual a $b.";
+            echo"<br><br>";
+
+            $a=$a+3;
+            echo"Agora A é igual á $a";
+            echo"<br><br>";
+
+            $b+=8;
+            echo"Agora B é igual a $b. ";
+            echo"<br><br>
+
+        s
+
+
+    
+        ?>
+    </body>
+</html>
