77adc75a6570e6639e0a27f9e26f33b062e41651
naiarasilva2 <naiara.osilva@aluno.edu.es.gov.br>
2025-10-01 11:00:18 -0300

    Update index.php

--- aula06/index.php
+++ aula06/index.php
@@ -8,13 +8,13 @@
 
         <?php
         $a=2;
-        $b=$a=5;
-        Echo "A e igual a $a e B e igual a $b.";
+        $b=$a+5;
+        echo "A e igual a $a e B e igual a $b.";
         echo "<br></br>";
 
         $a=$a+3;
         echo"Agora A é igual a $a.";
-        echo"<br><br>";
+        echo"<br></br>";
 
         $b+=8;
         echo "Agora A e igual $a";
@@ -23,7 +23,22 @@
         $b-=5;
         echo "Agora B e igual a $b.";
         echo "<br></br>";
+
+        $b*=2;
+        echo"Agora Multiplicando o valor de B por 2 e igual a $b.";
+        echo"<br></br>";
         
+        $b/=10;
+        echo"Agora Dividindo o valor de B por 10 e igual a $b.";
+        echo"<br></br>";
+
+        $b%=3
+        echo"O resto da divisão do valor de B dividido por 3 e igual a $b.";
+        echo"<br></br>";
+
+        $b.=10
+        echo"Concatenando o valor de B com o numero 10 fica igual a $b.";
+        echo"<br></br>";
         ?>
     </body>
 </html>
