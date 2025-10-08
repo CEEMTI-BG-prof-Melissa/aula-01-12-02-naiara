5daed83e437f8a4a6d0d8247fff6d74e51e7cc59
naiarasilva2 <naiara.osilva@aluno.edu.es.gov.br>
2025-10-08 10:43:42 -0300

    Update index.php

--- aula07/index.php
+++ aula07/index.php
@@ -1,4 +1,4 @@
-html>
+<html>
     <head>
         <title> Aula 07 PHP</title>
     </head>
@@ -10,35 +10,27 @@ html>
 
             $num=10;
             echo"Valor inicial : $num";
-            <br>
             
             $num+=5;
             echo"<br><br>A soma de 10 mais 5 e : $num";
-            <br>
 
             $num-=2;
             echo"<br>Apos subtrair 2 : $num";
-            <br>
 
             $num*=3;
             echo"<br>Apos multiplica por 3 : $num";
-            <br>
 
             $num/=2;
             echo"<br>Apos dividi por 2 : $num";
-            <br>
 
             ++$num;
             echo"<br><br>Apos Pré-incremento $num";
             echo"<br>Apos Pós-incremento : " .($num++);
-            <br>
 
             --$num;
             echo"<br>Apos Pré-encremento $num";
             echo"<br>Apos Pré-encremento : " .($num--);
             echo"<br><br>Valor final : $num"
-            
-
         ?>
     </body>
 </html>
